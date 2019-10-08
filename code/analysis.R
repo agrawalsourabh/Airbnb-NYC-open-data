c59a2493ba71c0ef0da6a02978ef1b01dc1d691f
agrawalsourabh <agrawal.sourabh644@gmail.com>
2019-10-08 13:32:50 +0530

    added new analysis

--- code/analysis.R
+++ code/analysis.R
@@ -9,6 +9,7 @@ library(ggplot2)
 library(e1071)
 library(scales)
 library(dplyr)
+library(tidyr)
 
 # importing dataset
 our.data = read.csv("input/AB_NYC_2019.csv", stringsAsFactors = F)
@@ -63,3 +64,86 @@ price_per_neighbourhood_group = our.data %>%
 #   Workspace saved
 #   */
   
+# Note: Property price at Manhattan is highest among them and Bronx has the lowest.
+
+# room_type
+unique(our.data$room_type)
+
+# these are categorical values, so change it to factors
+our.data$room_type = as.factor(our.data$room_type)
+
+# Note: Number of entire home/apt and Private room is much larger than shared room in our data set
+
+# room_type and price
+room_type_price_df = our.data %>%
+  group_by(room_type) %>%
+  summarise(median_price = median(price))
+
+# Note: Price of Entire home/apt is higher than that of Private Room or Shared room
+
+neigh_room.df = our.data %>%
+  group_by(neighbourhood_group, room_type) %>%
+  summarise(count_room = n())
+
+my_test = neigh_room.df %>%
+  group_by(neighbourhood_group) %>%
+  summarise(sum = sum(count_room))
+
+percent_vec = ifelse(neigh_room.df$neighbourhood_group == 'Bronx', 
+                     neigh_room.df$count_room/my_test$sum[1], percent_vec)
+percent_vec = ifelse(neigh_room.df$neighbourhood_group == 'Brooklyn', 
+                     neigh_room.df$count_room/my_test$sum[2], percent_vec)
+percent_vec = ifelse(neigh_room.df$neighbourhood_group == 'Manhattan', 
+                     neigh_room.df$count_room/my_test$sum[3], percent_vec)
+percent_vec = ifelse(neigh_room.df$neighbourhood_group == 'Queens', 
+                     neigh_room.df$count_room/my_test$sum[4], percent_vec)
+percent_vec = ifelse(neigh_room.df$neighbourhood_group == 'Staten Island', 
+                     neigh_room.df$count_room/my_test$sum[5], percent_vec)
+
+neigh_room.df$percentage = round(percent_vec*100, 2)
+
+# Note: From RoomType percentage Neighbourhood area, we found out that 
+# Manhattan - large number of entire home peresent as compared to Private and shared
+# Sim, Bronx and Queens - More number of private room present as compared to Entire home and shared
+# Broooklyn and Staten Island - Have almost same number of Entire or Private property available
+
+# Neighbourhood
+unique(our.data$neighbourhood)
+
+# longitude and latitude
+
+# number of reviews
+length(unique(our.data$number_of_reviews))
+
+# minimum_nights group by room_type
+min_night_room_type.df = our.data %>%
+  group_by(room_type) %>%
+  summarise(minimum_nights = median(minimum_nights))
+
+# room_type and reviews_per_month
+rt_rpm.df = our.data %>%
+  group_by(room_type) %>%
+  summarise(reviews_per_month = sum(reviews_per_month))
+
+rt_rpm.df$percentage[1] = round(rt_rpm.df$reviews_per_month[1] / sum(rt_rpm.df$reviews_per_month) *100
+                                , 2) 
+rt_rpm.df$percentage[2] = round(rt_rpm.df$reviews_per_month[2] / sum(rt_rpm.df$reviews_per_month) *100
+                                , 2) 
+rt_rpm.df$percentage[3] = round(rt_rpm.df$reviews_per_month[3] / sum(rt_rpm.df$reviews_per_month) *100
+                                , 2) 
+
+# room_type and number_of_reviews
+rt_total_reviews.df = our.data %>%
+  group_by(room_type) %>%
+  summarise(number_of_reviews = sum(number_of_reviews))
+
+rt_total_reviews.df$percentage[1] = round(rt_total_reviews.df$number_of_reviews[1] / sum(rt_total_reviews.df$number_of_reviews) *100
+                                , 2) 
+rt_total_reviews.df$percentage[2] = round(rt_total_reviews.df$number_of_reviews[2] / sum(rt_total_reviews.df$number_of_reviews) *100
+                                , 2) 
+rt_total_reviews.df$percentage[3] = round(rt_total_reviews.df$number_of_reviews[3] / sum(rt_total_reviews.df$number_of_reviews) *100
+                                , 2) 
+
+# /*
+#   Workspace saved
+#   */
--- code/graphical_analysis.R
+++ code/graphical_analysis.R
@@ -18,10 +18,114 @@ ggplot(data = our.data.num, mapping = aes(x = minimum_nights, y = price)) +
 # Neighbourhood_group and price
 our.data$neighbourhood_group
 our.data$price
-ggplot(data = our.data, mapping = aes(x = neighbourhood_group, 
-                                      y = median(price))) +
-  geom_bar(stat = 'identity', col = "#ad1105", alpha = 0.5) + 
-  ggtitle(" Bar Graph b/w Price and Neighbourhood Group") +
+
+ggplot(data = price_per_neighbourhood_group, mapping = aes(x = neighbourhood_group, 
+                                                           y = median_price)) + 
+  geom_bar(stat = 'identity', fill = '#5188e0', col = '#095ee6', alpha = 0.8) +
+  ggtitle("Median price per neighbourhood group") +
   xlab("Neighbourhood group") +
-  ylab("Price") +
-  scale_y_continuous(labels = dollar)
+  ylab("Median Price") +
+  scale_y_continuous(labels = dollar) +
+  geom_label(mapping = aes(label = paste('$', median_price)))
+
+ggsave("plots/PriceAndNeighbourhoodGroup.png")
+
+# room_type
+# plot a bar graph for room_type
+
+ggplot(data = our.data, mapping = aes(x = room_type)) +
+  geom_bar(fill = '#66edd4', col = '#07876f', alpha = 0.8) +
+  ggtitle("Room Type Analysis") +
+  xlab("Room Type") +
+  ylab("Count") +
+  geom_label(mapping = aes(label = ..count..), stat = 'count')
+
+ggsave("plots/RoomType_Bar.png")
+
+# room_type and price
+ggplot(data = room_type_price_df, mapping = aes(x = room_type, y = median_price)) +
+  geom_bar(stat = 'identity', fill='#8eba47', col='#2a4006') +
+  ggtitle("Room Type and their price (median)") +
+  xlab("Room Type") +
+  ylab("Median Price") +
+  scale_y_continuous(labels = dollar) + 
+  geom_label(mapping = aes(label = paste("$", median_price)))
+
+ggsave("plots/RoomType_Price.png")
+
+# which type of room is popular in neighbourhood_group
+ggplot(data = our.data, mapping = aes(x = neighbourhood_group, fill=room_type)) +
+  geom_bar()
+
+
+##
+
+ggplot(neigh_room.df, aes(room_type, percentage)) + 
+  geom_bar(stat = 'identity', fill='#f5ae62', col='#8a4f0f', alpha = 0.8, width = 0.5) + 
+  facet_grid(~ neighbourhood_group) +
+  geom_label(mapping = aes(label = paste(percentage, "%"))) +
+  ggtitle("Percentage of room type per neighbourhood area") +
+  xlab("Percentage") +
+  ylab("Room Type")
+
+ggsave("plots/RoomType_Perc_NG.png")
+
+
+# plot scatter plot between longitude and latitude
+ggplot(data = our.data, mapping = aes(x = latitude, y = longitude, col=price)) +
+  geom_point() +
+  scale_color_gradient(low = '#8af2a6', high = '#0b872c')
+
+ggsave("plots/long_lat_scatter.png")
+
+
+# number_of_reviews and minimum_nights with price
+# ggplot(data = our.data, mapping = aes(x = minimum_nights, y = price, col=price))+
+#   geom_point() +
+#   scale_color_gradient(low = '#83aef2', high = '#1a4c9c')
+
+# density graph for minimum nights
+ggplot(data = our.data, mapping = aes(x = minimum_nights)) +
+  geom_density()
+skewness(our.data$minimum_nights)
+
+# minimum_nights and room_type
+ggplot(data = min_night_room_type.df, mapping = aes(x = room_type, y = minimum_nights, 
+                                                    fill = minimum_nights)) +
+  geom_bar(stat = 'identity', width = 0.8) +
+  ggtitle("Room available for minimum nights") +
+  xlab('Room Type') +
+  ylab('Minimum Nights (Median)')+
+  scale_fill_gradient(low = "#f7968f", high = "#bd6a64") +
+  theme(legend.position = 'none')
+
+ggsave("plots/MinimumNight_RoomType_bar.png")
+
+# review_per_month based on room type
+ggplot(data = rt_rpm.df, mapping = aes(x = reorder(room_type, percentage), 
+                                       y = percentage)) +
+  geom_bar(stat = 'identity', col = '#228032', fill = '#77f28b', alpha = 0.8) +
+  coord_flip() +
+  ggtitle("Reviews per month based on room type") +
+  xlab("Room Type") +
+  ylab(" % Reviews per Month") +
+  geom_label(mapping = aes(label = paste(percentage, "%")))
+
+ggsave("plots/Permonth_Review_Perc_RoomType.png")
+
+# total reviews based on room type
+ggplot(data = rt_total_reviews.df, mapping = aes(x = reorder(room_type, percentage), 
+                                       y = percentage)) +
+  geom_bar(stat = 'identity', col = '#58aed6', fill = '#1e5a75', alpha = 0.8) +
+  coord_flip() +
+  ggtitle("Total number of reviews based on room type") +
+  xlab("Room Type") +
+  ylab(" % Reviews") +
+  geom_label(mapping = aes(label = paste(percentage, "%")))
+
+ggsave("plots/Review_Perc_RoomType.png")
+
+
+# /*
+#   Workspace saved
+#   */
